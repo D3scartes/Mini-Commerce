a1c5fb6634a997b3d9c5f63db2ea45de337dc515
Phoa Yohanes Theo Arvarrel <yohanes.274@users.noreply.github.com>
2025-09-26 08:30:19 +0700

    fixed rute halaman awal

--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,29 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 
-Route::redirect('/', '/dashboard');
-
 // Auth routes (Breeze)
 require __DIR__ . '/auth.php';
 
+// Paksa redirectnya ke product, karna guest bisa langsung liat produk
+Route::redirect('/', '/products');
+
+// Katalog PRODUK (PUBLIK)
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+
+
+// Beberapa page yg harus login
 Route::middleware(['auth', 'verified'])->group(function () {
-    // Dashboard
     Route::view('/dashboard', 'dashboard')->name('dashboard');
 
-    // Products (bisa diakses semua user yang login)
-    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-    Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
-
-    // Products (khusus admin)
+    // Admin-only untuk membuat produk 
     Route::middleware('admin')->group(function () {
         Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-        Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+        Route::post('/products',        [ProductController::class, 'store'])->name('products.store');
     });
 
-    // Profile (karena tidak memakai routes/profile.php)
+    // Profile
     Route::get('/profile',  [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::patch('/profile',[ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile',[ProfileController::class, 'destroy'])->name('profile.destroy');
 });
